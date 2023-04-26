61871b934d258f19486952ba740c9938033e6573
ALLI BAZEET <bazeet4love@gmail.com>
2023-04-27 00:35:50 +0100

    update from bazzly repo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
